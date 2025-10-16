68e3e68d66a7a0d6606faaa81285f2e07d443e3b
Lennart Novak <lennart.novak@schauinsland-reisen.de>
2025-10-16 10:35:59 +0200

    feat: wip

--- Vorlesungen/Vorlesung_3/main.py
+++ Vorlesungen/Vorlesung_3/main.py
@@ -4,14 +4,30 @@ import time
 from Vorlesungen.Vorlesung_2.fakultaet import fakultaet
 from Vorlesungen.Vorlesung_3.quicksort import quicksort
 
-# number = 5
-# print(f"Zahl:\t\t{number:_}")
-# startTime = time.perf_counter()
-# print(f"Fakultät:\t{fakultaet(number):_}")
-# endTime = time.perf_counter()
-# print(f"{endTime - startTime} Sekunden")
+def testFakultaet(number):
+    print(f"Zahl:\t\t{number:_}")
+    startTime = time.perf_counter()
+    print(f"Fakultät:\t{fakultaet(number):_}")
+    endTime = time.perf_counter()
+    print(f"{endTime - startTime} Sekunden")
 
-maxNumber = 30
-array = [random.randint(0, maxNumber) for _ in range(maxNumber)]
-print(array)
-print(quicksort(array))
+def testQuicksort(max):
+    array = [random.randint(0, max) for _ in range(max)]
+    sortedArray = array.copy()
+    sortedArray.sort()
+
+    print(f"Unsorted array:\t\t\t\t{array}")
+
+    startTime = time.perf_counter()
+    print(f"Quicksort array:\t\t\t{quicksort(array)}")
+    endTime = time.perf_counter()
+    print(f"{endTime - startTime} Sekunden")
+
+    startTime = time.perf_counter()
+    print(f"Quicksort sorted array:\t\t{quicksort(sortedArray)}")
+    endTime = time.perf_counter()
+    print(f"{endTime - startTime} Sekunden")
+
+
+# testFakultaet(5)
+testQuicksort(10)
